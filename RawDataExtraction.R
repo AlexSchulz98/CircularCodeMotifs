70a7a4a83de139fdd8f2ae6de328d5d01fd222da
Alex <alex.schulz1998@web.de>
2019-12-17 00:56:15 +0100

    codon and amino acid matrixes running and saving files to separate directories

--- RawDataExtraction.R
+++ RawDataExtraction.R
@@ -17,7 +17,7 @@ return(ar)
 
 #' fills table by counting how often codon A has been replaced with codon B
 #' @param ar matrix
-#' @param dim3 number of cds, 3D depth of array
+#' @param dim3 3D depth of array
 #' @param codonA DNAString from biological sequence
 #' @param codonB DNAString from modified sequence
 #' @param z current depth
@@ -25,14 +25,31 @@ codonCount = function(ar, dim3, seqA, seqB, z){
   
   codonsA = codons(seqA)
   codonsB = codons(seqB)
-  #print(length(codonsA))
   
   for (i in 1:length(codonsA)) {
     tmpArray =  array(data = 0,dim = c(64,64,dim3),dimnames = list(CODONS,CODONS,1:dim3))
-    #print(toString(codonsA[[i]]))
-    #print(toString(codonsB[[i]]))
     tmpArray[toString(codonsA[[i]]),toString(codonsB[[i]]),z] = 1
-    #print(tmpArray)
+    ar = ar+tmpArray
+  }
+  return(ar)
+}
+
+#' fills table by counting how often codon A has been replaced with codon B
+#' @param ar matrix
+#' @param dim3 3D depth of array
+#' @param codonA DNAString from biological sequence
+#' @param codonB DNAString from modified sequence
+#' @param z current depth
+aminoCount = function(ar, dim3, seqA, seqB, z){
+  
+  aminoA = translate(seqA)
+  aminoB = translate(seqB)
+  
+  for (i in 1:length(aminoA)) {
+    print(i)
+    
+    tmpArray =  array(data = 0,dim = c(21,21,dim3),dimnames = list(AMINOACIDS,AMINOACIDS,1:dim3))
+    tmpArray[toString(aminoA[i]),toString(aminoB[i]),z] = 1
     ar = ar+tmpArray
   }
   return(ar)
--- Testrun.R
+++ Testrun.R
@@ -1,55 +1,67 @@
 
 library(Biostrings)
 library(ccmotif)
+library(xlsx)
 source("CodeManipulation.R")
 source("Main.R")
 source("RawDataExtraction.R")
 
 # 64 codons
 CODONS = c("TTT", "TTC", "TTA", "TTG", "TCT", "TCC", "TCA", "TCG", "TAT", "TAC", "TAA", "TAG", "TGT", "TGC", "TGA", "TGG", "CTT","CTC", "CTA", "CTG", "CCT", "CCC", "CCA", "CCG", "CAT", "CAC", "CAA", "CAG", "CGT", "CGC", "CGA", "CGG", "ATT", "ATC", "ATA", "ATG", "ACT", "ACC", "ACA", "ACG", "AAT", "AAC", "AAA", "AAG", "AGT", "AGC", "AGA", "AGG", "GTT", "GTC" ,"GTA" ,"GTG", "GCT" ,"GCC" ,"GCA" ,"GCG" ,"GAT", "GAC", "GAA", "GAG", "GGT", "GGC", "GGA", "GGG")
-# 20 amino acids
-AMINOACIDS = c("A","R","N","D","C","Q","E","G","H","I","L","K","M","F","P","S","T","W","Y","V")
+# 20 amino acids + 1 *  for stop codons
+AMINOACIDS = c("A","R","N","D","C","Q","E","G","H","I","L","K","M","F","P","S","T","W","Y","V","*")
 
 seqSet = readDNAStringSet("cds/ena-sars.fasta") # DNA (RNA) sequence set
-setX = codes.c3[[26]] #Circular Code
+#setX = codes.c3[[26]] #Circular Code
 
 codon_table = generateEmptyTable(64,64,length(codes.c3), CODONS) #empty table for codons
-amino_table = generateEmptyTable(20,20,length(codes.c3), AMINOACIDS) #empty table for amino acisd
+amino_table = generateEmptyTable(21,21,length(codes.c3), AMINOACIDS) #empty table for amino acisd
 
 #---------------------------------------------
 
 start_time_global <- Sys.time()
 
 for (i in 1:length(codes.c3)) {
+  
+  start_time_code <- Sys.time()
+  
   setX = codes.c3[[i]]
   
   for (z in 1:length(seqSet)) {
+    
+    start_time_change <- Sys.time()
+    
     newSeq = main(seqSet[[z]], setX)
+    
     write.fasta(
       sequences = newSeq,
       names = paste("modified", names(seqSet[z])),
-      file.out = paste("output_SarsVirus_", setX[[1]], ".fasta", sep = ""),
+      file.out = paste("output_sequences/output_SarsVirus_", setX[[1]], ".fasta", sep = ""),
       open = "a",
       nbchar = 60,
       as.string = FALSE
     )
     
     outputMatrix_codons = codonCount(codon_table, length(codes.c3), seqSet[[z]], newSeq, i)
-    codon_table = outputMatrix
+    codon_table = outputMatrix_codons
+    
+    outputMatrix_amino = aminoCount(amino_table, length(codes.c3), seqSet[[z]], newSeq, i)
+    amino_table = outputMatrix_amino
     
     end_time_change <- Sys.time()
-    print(paste("Done with set", z, "/", length(seqSet)),"in",end_time_change - start_time_global)
+    print(paste("Done with set", z, "/", length(seqSet),"in",end_time_change - start_time_change))
   }
   
   end_time_code <- Sys.time()
-  print(paste("Done with circular code", i, "/", length(codes.c3)),"in",end_time_code - start_time_global)
+  print(paste("Done with circular code", i, "/", length(codes.c3),"in",end_time_code - start_time_code))
   
 }
 
-saveRDS(object = outputMatrix_codons, file = "codons_outputMatrix_SarsVirus_CircularCode26.RDS")
+saveRDS(object = outputMatrix_codons, file = "output_matrixes_codons/codons_outputMatrix_SarsVirus.RDS")
+write.xlsx(outputMatrix_codons, file ="output_matrixes_codons/codons_outputMatrix.xlsx", sheetName = "Sars Virus",col.names = TRUE, row.names = TRUE, append = TRUE)
 
-write.xlsx(outputMatrix_SarsVirus_CircularCode26, file ="codons_outputMatrix_Exc_SarsVirus_CircularCode26.xlsx", sheetName = "Sheet 1", 
-           +            col.names = TRUE, row.names = TRUE, append = FALSE)
+saveRDS(object = outputMatrix_aminos, file = "output_matrixes_aminos/aminos_outputMatrix_SarsVirus.RDS")
+write.xlsx(outputMatrix_aminos, file ="output_matrixes_aminos/aminos_outputMatrix.xlsx", sheetName = "Sars Virus",col.names = TRUE, row.names = TRUE, append = TRUE)
 
 end_time_global <- Sys.time()
 print(paste("Finished in",end_time_global - start_time_global))
